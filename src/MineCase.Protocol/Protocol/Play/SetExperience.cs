0c5b35786f5f36ce33b24bf3d8a899b265c6671c
BuildTools <unconfigured@null.spigotmc.org>
2019-11-23 20:40:27 +0200

    Fixed SetExperience packet

--- src/MineCase.Protocol/Protocol/Play/SetExperience.cs
+++ src/MineCase.Protocol/Protocol/Play/SetExperience.cs
@@ -9,7 +9,7 @@ namespace MineCase.Protocol.Play
 #if !NET46
     [Orleans.Concurrency.Immutable]
 #endif
-    [Packet(0x3F)]
+    [Packet(0x47)]
     public sealed class SetExperience : ISerializablePacket
     {
         [SerializeAs(DataType.Float)]
